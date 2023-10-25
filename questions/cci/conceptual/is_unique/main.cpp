01a8db0d016e96c3da9b9b104edfc43ba3489a29
Pratham <mishrapratham.codeit@gmail.com>
2023-10-26 03:48:18 +0530

    staged is_unique (concepts/hashtables) (unstable)

---
+++ questions/cci/conceptual/is_unique/main.cpp
@@ -0,0 +1,13 @@
+// **Is Unique:** Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures?
+#include <iostream>
+using namespace std;
+bool isUnique(string text)
+{
+    return false;
+}
+int main()
+{
+    string text = "Pratham";
+    cout << isUnique(text);
+    return 0;
+}
