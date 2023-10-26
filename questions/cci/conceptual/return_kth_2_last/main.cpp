4ec2f1e1063cdfbc24ae2f18b20ea2c77ea35f4b
Pratham <mishrapratham.codeit@gmail.com>
2023-10-26 08:49:29 +0530

    staged return_kth_2_last (concept/linkedlist) (unstable)

---
+++ questions/cci/conceptual/return_kth_2_last/main.cpp
@@ -0,0 +1,7 @@
+// **Return Kth to Last:** Implement an algorithm to find the kth to last element of a singly linked list.
+#include <iostream>
+using namespace std;
+int main()
+{
+    return 0;
+}
