6f6522b115c7b64a9cb98b284b9bdc0bc00a23e2
Pratham <mishrapratham.codeit@gmail.com>
2023-10-26 09:35:10 +0530

    staged delete_mid_node (unstable) (concept/linkedlist)

---
+++ questions/cci/conceptual/delete_mid_node/main.cpp
@@ -0,0 +1,11 @@
+// **Delete Middle Node:** Implement an algorithm to delete a node in the middle (i.e., any node but the first and last node, not necessarily the exact middle) of a singly linked list, given only access to that node.
+
+// Example:
+// Input: the node c from the linked list a->b->c->d->e->f
+// Result: nothing is returned, but the new linked list looks like a->b->d->e->f
+#include <iostream>
+using namespace std;
+int main()
+{
+    return 0;
+}
