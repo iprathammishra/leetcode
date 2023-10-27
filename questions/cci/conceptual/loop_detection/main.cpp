2988b382ddec7b133fa830a7a91ef64e5083b5b9
Pratham <mishrapratham.codeit@gmail.com>
2023-10-28 04:39:45 +0530

    loop_detection (stable)

--- questions/cci/conceptual/loop_detection/main.cpp
+++ questions/cci/conceptual/loop_detection/main.cpp
@@ -8,7 +8,52 @@
 // Output: C
 #include <iostream>
 using namespace std;
+class ListNode
+{
+public:
+    int data;
+    ListNode *next;
+    ListNode(int val) : data(val), next(nullptr) {}
+};
+ListNode *detectLoop(ListNode *n)
+{
+    ListNode *p1, *p2;
+    p1 = p2 = n;
+    while (p1 && p1->next)
+    {
+        p1 = p1->next;
+        p2 = p2->next->next;
+        if (p1 == p2)
+            break;
+    }
+
+    if (!p1 || !p1->next)
+        return nullptr;
+    p1 = n;
+    while (p1 != p2)
+    {
+        p1 = p1->next;
+        p2 = p2->next;
+    }
+    return p1;
+}
 int main()
 {
+    ListNode *A = new ListNode(1);
+    ListNode *B = new ListNode(2);
+    ListNode *C = new ListNode(3);
+    ListNode *D = new ListNode(4);
+    ListNode *E = new ListNode(5);
+    ListNode *F = new ListNode(6);
+
+    A->next = B;
+    B->next = C;
+    C->next = B;
+    D->next = E;
+    E->next = F;
+    F->next = C;
+
+    cout << "Loop starts from: " << detectLoop(A)->data;
+
     return 0;
 }
