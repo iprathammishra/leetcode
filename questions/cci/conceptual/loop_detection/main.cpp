0e834e2ddde0c04f12516bf3479480c150c7f5fc
Pratham <mishrapratham.codeit@gmail.com>
2023-10-28 03:25:32 +0530

    staged loop_detection (concepts/linkedlist) (unstalbe)

---
+++ questions/cci/conceptual/loop_detection/main.cpp
@@ -0,0 +1,14 @@
+// **Loop Detection:** Given a circular linked list, implement an algorithm that returns the node at the beginning of the loop.
+
+// Definition:
+// Circular linked list: A (corrupt) linked list in which a node's next pointer points to an earlier node, so as to make a loop in the linked list.
+
+// Example:
+// Input: A->B->C->D->E->C [the same C as earlier]
+// Output: C
+#include <iostream>
+using namespace std;
+int main()
+{
+    return 0;
+}
