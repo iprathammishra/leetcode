8a351da3cc76c3aef1407a26df15fd7da57f53cc
Pratham <mishrapratham.codeit@gmail.com>
2023-10-31 08:47:41 +0530

    staged string_rotation (concepts/hashtables) (unstable)

---
+++ questions/cci/conceptual/string_rotation/main.cpp
@@ -0,0 +1,7 @@
+// **String Rotation:** Assume you have method isSubstring which checks if one word is a substring of another. Given two strings, S1 and S2, write a code to check if S2 is a rotation of S1 using only one call to isSubstring (e.g., "waterbottle" is a rotation of "erbottlewat").
+#include <iostream>
+using namespace std;
+int main()
+{
+    return 0;
+}
