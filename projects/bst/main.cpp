b6829cb7c50d0e0f651508218327e425aa5dea55
Pratham <mishrapratham.codeit@gmail.com>
2023-11-01 05:18:15 +0530

    some updates to BST project

--- projects/bst/main.cpp
+++ projects/bst/main.cpp
@@ -1,6 +1,21 @@
+// A `Binary Search Tree (BST)` is a collection of nodes arranged in a way where they maintain BST properties.
+
+// - Every node in the `left subtree` has a value `less` than the value of its parent node.
+// - Every node in the `right subtree` has a value `greater than or equal to` the value of its parent node.
 #include <iostream>
 using namespace std;
+class TreeNode
+{
+public:
+    int data;
+    TreeNode *left, *right;
+    TreeNode(int val) : data(val), left(nullptr), right(nullptr) {}
+};
+class BinarySearchTree
+{
+};
 int main()
 {
+    int choice;
     return 0;
 }
