97e4a618214c411f62ed38a800cf57313a464d61
Pratham <mishrapratham.codeit@gmail.com>
2023-10-31 09:15:26 +0530

    string_rotation (stable)

--- questions/cci/conceptual/string_rotation/main.cpp
+++ questions/cci/conceptual/string_rotation/main.cpp
@@ -1,7 +1,13 @@
 // **String Rotation:** Assume you have method isSubstring which checks if one word is a substring of another. Given two strings, S1 and S2, write a code to check if S2 is a rotation of S1 using only one call to isSubstring (e.g., "waterbottle" is a rotation of "erbottlewat").
 #include <iostream>
+#include <string>
 using namespace std;
+bool isRotationSubstring(const string &str1, const string &str2)
+{
+    return (str1.length() != str2.length()) ? false : ((str1 + str1).find(str2) != string::npos);
+}
 int main()
 {
+    cout << isRotationSubstring("waterbottle", "terbottlewa");
     return 0;
 }
