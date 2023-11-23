0f7c8562d224c1ab95c9bb3d32a1c412fb8ab818
Pratham <mishrapratham.codeit@gmail.com>
2023-11-23 16:50:19 +0530

    added question

--- questions/daa/chap2/c-2_1.cpp
+++ questions/daa/chap2/c-2_1.cpp
@@ -1,3 +1,4 @@
+// A double-ended queue, or deque, is a list that allows for insertions and removals at either its head or its tail. Describe a way to implement a deque using a doubly linked list, so that every operation runs in O(1) time.
 #include <iostream>
 int main()
 {
