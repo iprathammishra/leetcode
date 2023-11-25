e43241edb12be5d014d37dfae22879d7673e4648
Pratham <mishrapratham.codeit@gmail.com>
2023-11-25 16:41:59 +0530

    added c-2_7.cpp (unstable)

---
+++ questions/daa/chap2/c-2_7.cpp
@@ -0,0 +1,5 @@
+// Design algorithms for the following operations for a node v in a binary tree T:
+
+// preOrderNext(v): return the node visited after v in a preorder traversal of T.
+// inOrderNext(v): return the node visited after v in an inorder traversal of T.
+// postOrderNext(v): return the node visited after v in a postorder traversal of T.
