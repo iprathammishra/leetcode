e690846172d02203bea1332cf178949059e41f75
Pratham <mishrapratham.codeit@gmail.com>
2024-01-10 10:19:07 +0000

    raw

---
+++ dsa/cracking/moderate/intersection.cpp
@@ -0,0 +1,11 @@
+// 3. **Intersection:** Given two straight line segements (represented as a start point and an end point), computer the point of intersection, if any.
+#include <iostream>
+int main()
+{
+    return 0;
+}
+
+// y2 - y1 / x2 - x1 = m
+// y - y1 = m (x - x1)
+// x = b1c2 - b2c1/a1b2 - a2b1
+// y = a2c1 - a1c2/a1b2 - a2b1
