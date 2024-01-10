b00a66f61ff457f59837331bf88bc24f37ed18af
Pratham <mishrapratham.codeit@gmail.com>
2024-01-10 13:23:02 +0000

    added file num_max.cpp

--- dsa/cracking/moderate/num_max.cpp
+++ dsa/cracking/moderate/num_max.cpp
@@ -1,1 +1,15 @@
 // 7. **Number Max:** Write a method that finds the maximum of two numbers. You should not use if-else or any other comparison operator.
+#include <iostream>
+int findMax(const int& a, const int& b)
+{
+    int diff = a - b;
+    int signDiff = (diff >> 31) & 1;
+    return (a - signDiff*diff);
+}
+int main()
+{
+    std::cout << findMax(45, 5) << std::endl;
+    return 0;
+}
+
+// https://chat.openai.com/share/00afc13f-a961-4f62-b53e-8668ce107856
