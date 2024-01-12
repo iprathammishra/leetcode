d1ce59b4db823e4e79023d1c0be85554b29a9275
Pratham <mishrapratham.codeit@gmail.com>
2024-01-12 13:11:35 +0530

    raw

---
+++ dsa/cracking/moderate/pond_siz.cpp
@@ -0,0 +1,16 @@
+// 19. **Pond Sizes:** You have an integer matrix representing a plot of land, where the value at that location represents the height above sea level. A value of zero indicates water. A pond is a region of water connected vertically, horizontally, or diagonally. The size of the pond is the total number of connected water cells. Write a method to compute the sizes of all ponds in the matrix.
+
+// Example: <br>
+// Input:
+
+// 0 2 1 0
+// 0 1 0 1
+// 1 1 0 1
+// 0 1 0 1
+
+// Output: 2, 3, 1 (in any order)
+#include <iostream>
+int main()
+{
+    return 0;
+}
