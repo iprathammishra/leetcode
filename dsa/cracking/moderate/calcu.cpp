6af36201d89e54fc0ed4ce17c5bae2f909663ab4
Pratham <mishrapratham.codeit@gmail.com>
2024-01-15 16:18:16 +0530

    raw

--- dsa/cracking/moderate/calcu.cpp
+++ dsa/cracking/moderate/calcu.cpp
@@ -1,10 +1,16 @@
-// 26. **Calculator:** Given an arithmetic equation consisting of positive integers, +, -, \* and / (no parentheses), compute the result.
+// 26. **Calculator:** Given an arithmetic equation consisting of positive integers, +, -, * and / (no parentheses), compute the result.
 
 // Example:
-// Input: 2*3+5/6*3+15 <br>
+// Input: 2*3+5/6*3+15
 // Output: 23.5
 #include <iostream>
+#include <vector>
+#include <string>
+int calculateExpression(std::string &ex)
+{
+}
 int main()
 {
+    std::string expression = "2*3+5/6*3+15";
     return 0;
 }
