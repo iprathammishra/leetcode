b7ee8b9e30421cde248bc82d3e684b9631f34abb
Pratham <mishrapratham.codeit@gmail.com>
2024-01-15 14:57:35 +0530

    added rand7_rand5.cpp file

--- dsa/cracking/moderate/rand7_rand5.cpp
+++ dsa/cracking/moderate/rand7_rand5.cpp
@@ -1,6 +1,22 @@
 // 23. **Rand7 from Rand5:** Implement a method rand7() given rand5(). That is, given a method that generates a random number between 0 and 4 (inclusive), write a method that generates a random number between 0 and 6 (inclusive).
 #include <iostream>
+int rand5()
+{
+    return std::rand() % 5;
+}
+int rand7()
+{
+    int num;
+    do
+    {
+        num = 5 * rand5() + rand5();
+    } while (num >= 21);
+
+    return num % 7;
+}
 int main()
 {
+    for (int i = 0; i <= 9; ++i)
+        std::cout << rand7() << " ";
     return 0;
 }
