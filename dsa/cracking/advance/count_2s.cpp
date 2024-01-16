3a97322940eeec126312d4c180200065a624b9f9
Pratham <mishrapratham.codeit@gmail.com>
2024-01-16 19:24:01 +0530

    rawe

---
+++ dsa/cracking/advance/count_2s.cpp
@@ -0,0 +1,11 @@
+// 6. **Count of 2s:** Write a method to count the number of 2s that appear in all the numbers between 0 and n (inclusive).
+
+// Example:
+
+// Input: 25
+// Output: 9 (2, 12, 20, 21, 22, 23, 24 and 25. Note that 22 counts for two 2s).
+#include <iostream>
+int main()
+{
+    return 0;
+}
