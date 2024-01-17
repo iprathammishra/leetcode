2780d915bfc6a874b7e928f5ebc785cf6f655d4b
Pratham <mishrapratham.codeit@gmail.com>
2024-01-17 22:25:08 +0530

    added circus_tower.cpp file

--- dsa/cracking/advance/circus_tow.cpp
+++ dsa/cracking/advance/circus_tow.cpp
@@ -1,11 +1,35 @@
 // 8. **Circus Tower:** A circus is designing a tower routine consisting of people standing atop one another's shoulders. For practical and aesthetic reasons, each person must be both shorter and lighter than the person below him or her. Given the heights and weights of each person in the circus, write a method to compute the largest possible number of people in such a tower.
 
-// Example: <br>
-// Input (ht, wt): (65, 100) (70, 150), (56, 90) (75, 190) (60, 95) (68, 110) <br>
-// Output: The longest tower is length of 6 and includes from top to bottom:<br>
+// Example:
+// Input (ht, wt): (65, 100) (70, 150), (56, 90) (75, 190) (60, 95) (68, 110)
+// Output: The longest tower is length of 6 and includes from top to bottom:
 // (56, 90) (60, 95) (65, 100) (68, 110) (70, 150) (75, 190)
 #include <iostream>
+#include <unordered_map>
+#include <vector>
+#include <algorithm>
+std::vector<std::pair<int, int>> circusTower(const std::unordered_map<int, int> &ht_wt)
+{
+    std::vector<int> rVec;
+    for (const auto &pair : ht_wt)
+        rVec.push_back(pair.first);
+    std::sort(rVec.begin(), rVec.end());
+    std::vector<std::pair<int, int>> r;
+    for (const int i : rVec)
+        r.emplace_back(i, ht_wt.at(i));
+    return r;
+}
 int main()
 {
+    std::unordered_map<int, int> ht_wt = {
+        {65, 100},
+        {70, 150},
+        {56, 90},
+        {75, 190},
+        {60, 95},
+        {68, 110}};
+    std::vector<std::pair<int, int>> r = circusTower(ht_wt);
+    for (const auto &pair : r)
+        std::cout << "{" << pair.first << ", " << pair.second << "}" << std::endl;
     return 0;
 }
