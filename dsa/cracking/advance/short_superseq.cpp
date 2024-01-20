5b0f36c92d46c20d0b433d659776a4855a36a100
Pratham <mishrapratham.codeit@gmail.com>
2024-01-20 13:42:12 +0530

    raw

---
+++ dsa/cracking/advance/short_superseq.cpp
@@ -0,0 +1,10 @@
+// 18. **Shortest Supersequence:** You are given two arrays, one shorter (with all distinct elements) and one longer. Find the shortest subarray in the longer array that contains all the elements in the shorter array. The items can appear in any order.
+
+// Example:<br>
+// Input: {1, 5, 9} | {7, 5, 9, 0, 2, 1, 3, (5, 7, 9, 1), 1, 5, 8, 8, 9, 7}<br>
+// Output: [7, 10] (the underlined portion above)
+#include <iostream>
+int main()
+{
+    return 0;
+}
