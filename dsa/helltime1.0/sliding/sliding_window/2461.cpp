ea38d97f0bfa462bacf74bfb980d8d86ff40dffd
Pratham <mishrapratham.codeit@gmail.com>
2024-02-01 01:12:32 +0530

    added 2461.cpp

---
+++ dsa/helltime1.0/sliding/sliding_window/2461.cpp
@@ -0,0 +1,28 @@
+class Solution
+{
+public:
+    long long maximumSubarraySum(vector<int> &nums, int k)
+    {
+        int n = nums.size();
+        long long sum = 0, max_sum = 0;
+        map<int, int> check;
+        for (int i = 0; i < k - 1; ++i)
+        {
+            sum += nums[i];
+            ++check[nums[i]];
+        }
+        for (int i = k - 1; i < n; ++i)
+        {
+            ++check[nums[i]];
+            sum += nums[i];
+            if (check.size() == k)
+                max_sum = max(sum, max_sum);
+            if (check[nums[i - k + 1]] == 1)
+                check.erase(nums[i - k + 1]);
+            else
+                --check[nums[i - k + 1]];
+            sum -= nums[i - k + 1];
+        }
+        return max_sum;
+    }
+};
