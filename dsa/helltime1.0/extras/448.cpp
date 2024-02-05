6288128255c16f6a26f4c75472ed464cfb8f874b
Pratham <mishrapratham.codeit@gmail.com>
2024-02-05 17:09:46 +0530

    added 448.cpp

---
+++ dsa/helltime1.0/extras/448.cpp
@@ -0,0 +1,19 @@
+class Solution
+{
+public:
+    vector<int> findDisappearedNumbers(vector<int> &nums)
+    {
+        vector<int> res;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            int val = abs(nums[i]);
+            if (1 <= val && val <= nums.size())
+                if (nums[val - 1] > 0)
+                    nums[val - 1] *= -1;
+        }
+        for (int i = 0; i < nums.size(); i++)
+            if (nums[i] > 0)
+                res.push_back(i + 1);
+        return res;
+    }
+};
