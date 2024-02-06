30efbacea895719d637594aa111f609397dd2b5f
Pratham <mishrapratham.codeit@gmail.com>
2024-02-06 08:49:45 +0000

    added 2273.cpp

---
+++ dsa/helltime1.0/extras/2273.cpp
@@ -0,0 +1,25 @@
+class Solution {
+private:
+    string createSignature(string word) {
+        sort(word.begin(), word.end());
+        return word;
+    }
+public:
+    vector<string> removeAnagrams(vector<string>& words) {
+        vector<string> res;
+        res.push_back(words[0]);
+
+        string previousWord = createSignature(words[0]);
+
+        for (size_t i = 1; i < words.size(); i++)
+        {
+            string currentWord = createSignature(words[i]);
+            if (previousWord != currentWord)
+            {
+                res.push_back(words[i]);
+                previousWord = currentWord;
+            }
+        }
+        return res;
+    }
+};
