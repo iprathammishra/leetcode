6bef517d62f74783ee501ffd2fa34e1e9c4efadd
Pratham <mishrapratham.codeit@gmail.com>
2024-02-20 17:10:27 +0530

    added 31.cpp

---
+++ dsa/helltime1.0/backtracking/backtracking/31.cpp
@@ -0,0 +1,25 @@
+class Solution
+{
+public:
+    void nextPermutation(vector<int> &nums)
+    {
+        int n = nums.size();
+        int i = n - 2;
+        while (i >= 0 && nums[i] >= nums[i + 1])
+        {
+            --i;
+        }
+        if (i >= 0)
+        {
+            for (int j = n - 1; j > i; --j)
+            {
+                if (nums[j] > nums[i])
+                {
+                    std::swap(nums[i], nums[j]);
+                    break;
+                }
+            }
+        }
+        std::reverse(nums.begin() + i + 1, nums.end());
+    }
+};
