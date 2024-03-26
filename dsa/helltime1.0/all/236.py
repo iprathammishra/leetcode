c6c928384ee26596ce5a5deac80e6b95528cbb1d
Pratham <mishrapratham.codeit@gmail.com>
2024-03-27 02:15:06 +0530

    added 236.py

---
+++ dsa/helltime1.0/all/236.py
@@ -0,0 +1,16 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if root is None or root == p or root == q:
+            return root
+        leftLCA = self.lowestCommonAncestor(root.left, p, q)
+        rightLCA = self.lowestCommonAncestor(root.right, p, q)
+        if leftLCA and rightLCA:
+            return root
+        return leftLCA if leftLCA else rightLCA
