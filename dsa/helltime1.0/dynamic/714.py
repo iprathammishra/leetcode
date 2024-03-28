439632d5922dfe35593b0ba61ba5f93f64e29af5
Pratham <mishrapratham.codeit@gmail.com>
2024-03-28 18:52:17 +0530

    added 714.py

---
+++ dsa/helltime1.0/dynamic/714.py
@@ -0,0 +1,6 @@
+class Solution:
+    def maxProfit(self, prices: List[int], fee: int) -> int:
+        cash, hold = 0, -prices[0]
+        for price in prices[1:]:
+            cash, hold = max(cash, hold + price - fee), max(hold, cash - price)
+        return cash
