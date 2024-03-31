08e63807682359d5ed3ca9dbde4823d58b83cf0a
Pratham <mishrapratham.codeit@gmail.com>
2024-03-31 09:30:25 +0530

    added 2444.py

---
+++ dsa/helltime1.0/extras/2444.py
@@ -0,0 +1,13 @@
+class Solution:
+    def countSubarrays(self, nums: List[int], minK: int, maxK: int) -> int:
+        l_far = l_near = right = -1
+        res = 0
+        for index, num in enumerate(nums):
+            if not minK <= num <= maxK:
+                l_far = index
+            if num == minK:
+                l_near = index
+            if num == maxK:
+                right = index
+            res += max(0, min(l_near, right) - l_far)
+        return res
