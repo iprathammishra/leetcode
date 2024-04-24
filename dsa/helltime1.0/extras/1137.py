478db8ab745557fe698ded967fc834420d0a8b4a
Pratham <mishrapratham.codeit@gmail.com>
2024-04-24 17:57:07 +0530

    an update

--- dsa/helltime1.0/extras/1137.py
+++ dsa/helltime1.0/extras/1137.py
@@ -1,7 +1,6 @@
 class Solution:
     def tribonacci(self, n: int) -> int:
-        if n == 0:
-            return 0
-        if n == 1 or n == 2:
-            return 1
-        return self.tribonacci(n-3)+self.tribonacci(n-2)+self.tribonacci(n-1)
+        ans = [0, 1, 1]
+        for i in range(3, n+1):
+            ans.append(ans[i-1]+ans[i-2]+ans[i-3])
+        return ans[n]
